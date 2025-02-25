c999181e06fec2c3027977ec8274a1e18c7db998
parthR1 <22f3002726@ds.study.iitm.ac.in>
2025-02-25 20:46:10 +0530

    fix: Modify code to work with supabase and sequelize

--- index.js
+++ index.js
@@ -1,60 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./lib/sequelize.js");
+const User = require("./models/user.js");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Sample data
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database connected and synced.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to database", error);
+  });
 
-const users = [
-  {
-    id: 1,
-    username: "octocat",
-    name: "The Octocat",
-    repoCount: 8,
-    location: "San Francisco",
-  },
-  {
-    id: 2,
-    username: "torvalds",
-    name: "Linus Torvalds",
-    repoCount: 25,
-    location: "Portland",
-  },
-  {
-    id: 3,
-    username: "gaearon",
-    name: "Dan Abramov",
-    repoCount: 50,
-    location: "London",
-  },
-  {
-    id: 4,
-    username: "addyosmani",
-    name: "Addy Osmani",
-    repoCount: 42,
-    location: "Mountain View",
-  },
-  {
-    id: 5,
-    username: "tj",
-    name: "TJ Holowaychuk",
-    repoCount: 150,
-    location: "Victoria",
-  },
-];
-
-app.get("/users", (req, res) => {
-  res.json({ users });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json({ users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to fetch all users" });
+  }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   let targetId = parseInt(req.params.id);
-  let targetUser = users.find((user) => user.id === targetId);
-  res.json({ User: targetUser });
+  try {
+    let targetUser = await User.findByPk(targetId);
+
+    if (targetUser) {
+      res.json({ targetUser });
+    } else {
+      res.status(404).json({ message: "User not found." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch user" });
+  }
 });
 
 app.listen(PORT, () => {
